6605225de3d83f04a4ece4d5f40229b10d8964af
1ou1s123 <yunenqian@gmail.com>
2025-10-15 12:07:36 +0800

    week05

---
+++ week05-3a.cpp
@@ -0,0 +1,17 @@
+/// week05-3a.cpp —— 合併的 Part1 / Part2 Input / Output
+/// CPE 這次的第2題 UVA 483 Word Scramble 把字弄反
+
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string line;  /// Part 1: Input
+    while (getline(cin, line)) {   /// 讀入一行，一直讀到 Ctrl+Z 結束
+        /// Part 2: Output
+        cout << line << endl;      /// 先隨便印出來
+    }
+}
+
